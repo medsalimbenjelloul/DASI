6969760d15f0b58d1630ff1e9c40e45bf00bac6c
Adam <32632145+Andy104@users.noreply.github.com>
2019-04-02 17:40:38 +0200

    Skeleton 2.1

--- src/agents/UserAgent.java
+++ src/agents/UserAgent.java
@@ -1,8 +1,58 @@
 package agents;
 
+import java.util.ArrayList;
+
+import core.Listener;
+import jade.core.AID;
 import jade.core.Agent;
+import view.Window;
 
 public class UserAgent extends Agent {
 	private static final long serialVersionUID = -4477365032710457655L;
+	
+	private ArrayList<Listener> observers = null;
+	private ArrayList<AID> aids = null;
+	
+	private Window gui = null;
+	
+	public UserAgent() {
+		this.observers = new ArrayList<>();
+		this.aids = new ArrayList<>();
+	}
+
+	@Override
+	protected void setup() {
+		System.out.println("UserAgent.setup");
+		
+//		addBehaviour(new Behaviour());
+		
+		gui = new Window(this);
+		addObserver(gui);
+	}
+
+	@Override
+	protected void takeDown() {
+		System.out.println("UserAgent.takeDown");
+	}
+	
+	public void addObserver(Listener observer) {
+		if (!observers.contains(observer)) {
+			this.observers.add(observer);
+		}
+	}
+	
+	public ArrayList<Listener> getObservers() {
+		return this.observers;
+	}
+	
+	public void addAID(AID aid) {
+		if (!aids.contains(aid)) {
+			this.aids.add(aid);
+		}
+	}
+	
+	public ArrayList<AID> getAIDs() {
+		return this.aids;
+	}
 
 }
